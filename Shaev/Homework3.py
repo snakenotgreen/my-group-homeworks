99eec692b4266d0d6322cd6ca350dd14946ebff5
snakenotgreen <72945281+snakenotgreen@users.noreply.github.com>
2020-11-09 16:44:07 +0200

    Update Homework3.py

--- Shaev/Homework3.py
+++ Shaev/Homework3.py
@@ -3,10 +3,13 @@ with open('users.txt', 'r+', encoding='utf-8') as f:
     word = input('Здравствуйте, вы проходили регистрацию на нашем ресурсе? [Да], [Нет]\n').lower()
     if word == 'да':
         login = input('Введите логин от вашего аккаунта')
-        password = input('Введите пароль от вашего аккаунта')
-        text = f.read()
-        if login in text and password in text:
-            print('Вход в аккаунт успешно осуществлён')
+        text = f.read().split()
+        if login in text:
+            password = input('Введите пароль от вашего аккаунта')
+            if password in text:
+                print('Вход в аккаунт успешно осуществлён')
+            else:
+                print('Ошибка при входе')
         else:
             print('Ошибка при входе')
     elif word == 'нет':
