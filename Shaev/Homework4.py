ea28885ec3409ff4eacc7b5a9e885474de9061b8
snakenotgreen <72945281+snakenotgreen@users.noreply.github.com>
2020-11-13 20:47:40 +0200

    Update Homework4.py

--- Shaev/Homework4.py
+++ Shaev/Homework4.py
@@ -1,8 +1,7 @@
 # Первая задача
-x = 4
 a = [50, 30, 14, 15, 1]
-for i in range(x-1):
-    for j in range(x-1):
+for i in range(len(a)-1):
+    for j in range(len(a)-1):
         if a[j] > a[j+1]:
             a[j], a[j+1] = a[j+1], a[j]
 print(a)
